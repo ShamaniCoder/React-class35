6504e8ab33f4b1a724bcba077affdac20483dfde
ShamaniCoder <ertugrulaktn@gmail.com>
2022-05-05 14:15:08 +0200

    add some css

--- week1/project/ecommerce/src/components/Products.js
+++ week1/project/ecommerce/src/components/Products.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+
 import ProductCard from "./ProductCard";
 import productsData from "../fake-data/all-products";
 
@@ -31,15 +31,4 @@ function Products({ selectedCategory }) {
 
 export default Products;
 
-// {
-//     id: 1,
-//     title: "FAKE: Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-//     price: 109.95,
-//     description:
-//       "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-//     category: "men's clothing",
-//     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-//     rating: {
-//       rate: 3.9,
-//       count: 120,
-//     }
+
